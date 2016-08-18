a7f3a3256506439b7a968403f7800863d56c2428
Arukim <smelovn@gmail.com>
2016-08-18 21:09:35 +0300

    command handler

--- login/gameServer.go
+++ login/gameServer.go
@@ -2,10 +2,10 @@ package login
 
 import (
 	"encoding/json"
-	"fmt"
-	"io"
 	"log"
 	"net/http"
+
+	"github.com/arukim/galaxy/network"
 
 	"golang.org/x/net/websocket"
 )
@@ -18,48 +18,38 @@ type Room struct {
 type GameServer struct {
 	pattern string
 	rooms   []Room
+	router  *network.Router
 }
 
 func NewGameServer(pattern string) *GameServer {
 	s := GameServer{
 		pattern: pattern,
 	}
+
 	s.rooms = []Room{{Name: "red room", ID: 0}}
+
+	s.router = network.NewRouter([]network.CommandHandler{
+		{
+			Name: "join",
+			Handler: func(Data *json.RawMessage) []byte {
+				resp, _ := json.Marshal("joined the room")
+				return resp
+			},
+		},
+		{
+			Name: "rooms",
+			Handler: func(Data *json.RawMessage) []byte {
+				resp, _ := json.Marshal(s.rooms)
+				return resp
+			},
+		},
+	})
+
 	return &s
-}
-
-func (gs *GameServer) onConnected(ws *websocket.Conn) {
-	defer ws.Close()
-
-	fmt.Println("client connected")
-	for true {
-		var msg string
-		var resp []byte
-		err := websocket.JSON.Receive(ws, &msg)
-
-		if err == io.EOF {
-			fmt.Println("client disconnected")
-			return
-		}
-
-		fmt.Println(msg)
-		switch msg {
-		case "rooms":
-			resp, _ = json.Marshal(gs.rooms)
-		case "join":
-			resp, _ = json.Marshal("connected")
-		case "info":
-			resp, _ = json.Marshal("Welcome galaxy game server")
-		default:
-			resp, _ = json.Marshal("Unknown command. Allowed commands: rooms, info. Use json.")
-		}
-
-		ws.Write(resp)
-	}
 }
 
 func (gs *GameServer) Listen() {
 	log.Printf("GameServer is started on %v\n", gs.pattern)
 
-	http.Handle(gs.pattern, websocket.Handler(gs.onConnected))
+	http.Handle(gs.pattern, websocket.Handler(gs.router.Listen))
 }
